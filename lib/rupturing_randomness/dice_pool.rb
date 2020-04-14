c856e3ce9d9ba5e1b7502dbc80e4603257164305
JD Huntington <jd@itsthehuntingtons.com>
2020-04-14 11:34:25 -0700

    Cheat for now

--- lib/rupturing_randomness/dice_pool.rb
+++ lib/rupturing_randomness/dice_pool.rb
@@ -2,4 +2,8 @@ class DicePool
   def initialize *dice
     @dice = dice
   end
+
+  def possibilities
+    [DieResult.new(2, 4), DieResult.new(3, 4), DieResult.new(3, 4), DieResult.new(4, 4)]
+  end
 end
