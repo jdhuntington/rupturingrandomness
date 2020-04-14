593b7fc07f26b2ddfaf81268419bf03c54a86f37
JD Huntington <jd@itsthehuntingtons.com>
2020-04-14 10:58:16 -0700

    test passess

--- lib/rupturing_randomness/die.rb
+++ lib/rupturing_randomness/die.rb
@@ -1,2 +1,10 @@
 class Die
+  def initialize sides, explosions
+    @sides = sides
+    @explosions = explosions
+  end
+
+  def possibilities
+    (1..@sides).to_a
+  end
 end
--- spec/rupturing_randomness/die_spec.rb
+++ spec/rupturing_randomness/die_spec.rb
@@ -2,13 +2,14 @@ require "minitest/autorun"
 require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'rupturing_randomness'))
 
 describe Die do
-  before do
-    @die = Die.new
-  end
 
-  describe "existence" do
-    it "must exist" do
-      assert !@die.nil?
+  describe "possibilities with no explosions" do
+    before do
+      @die = Die.new(6, 0)
+    end
+
+    it 'has a chance to hit all 6 numbers evenly' do
+      assert_equal (1..6).to_a, @die.possibilities
     end
   end
 end
